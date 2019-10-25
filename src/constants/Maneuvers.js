b06c6d19d898755782c2ae302282017ea9e49d8c
Akhil <akhil.kumar@sproutlogix.com>
2019-10-25 18:18:37 +0530

    Refactor code.

---
+++ src/constants/Maneuvers.js
@@ -0,0 +1,134 @@
+export const Maneuvers = [
+  {
+    value: 'ARRIVE',
+    display: 'ARRIVE',
+  },
+  {
+    value: 'ARRIVE_LEFT',
+    display: 'ARRIVE LEFT',
+  },
+  {
+    value: 'ARRIVE_RIGHT',
+    display: 'ARRIVE RIGHT',
+  },
+  {
+    value: 'DEPART',
+    display: 'DEPART',
+  },
+  {
+    value: 'STRAIGHT',
+    display: 'STRAIGHT',
+  },
+  {
+    value: 'KEEP_RIGHT',
+    display: 'KEEP RIGHT',
+  },
+  {
+    value: 'BEAR_RIGHT',
+    display: 'BEAR RIGHT',
+  },
+  {
+    value: 'TURN_RIGHT',
+    display: 'TURN RIGHT',
+  },
+  {
+    value: 'SHARP_RIGHT',
+    display: 'SHARP RIGHT',
+  },
+  {
+    value: 'KEEP_LEFT',
+    display: 'KEEP LEFT',
+  },
+  {
+    value: 'BEAR_LEFT',
+    display: 'BEAR LEFT',
+  },
+  {
+    value: 'TURN_LEFT',
+    display: 'TURN LEFT',
+  },
+  {
+    value: 'SHARP_LEFT',
+    display: 'SHARP LEFT',
+  },
+  {
+    value: 'MAKE_UTURN',
+    display: 'MAKE UTURN',
+  },
+  {
+    value: 'ENTER_MOTORWAY',
+    display: 'ENTER MOTORWAY',
+  },
+  {
+    value: 'ENTER_FREEWAY',
+    display: 'ENTER FREEWAY',
+  },
+  {
+    value: 'ENTER_HIGHWAY',
+    display: 'ENTER HIGHWAY',
+  },
+  {
+    value: 'TAKE_EXIT',
+    display: 'TAKE EXIT',
+  },
+  {
+    value: 'MOTORWAY_EXIT_LEFT',
+    display: 'EXIT LEFT',
+  },
+  {
+    value: 'MOTORWAY_EXIT_RIGHT',
+    display: 'EXIT RIGHT',
+  },
+  {
+    value: 'TAKE_FERRY',
+    display: 'TAKE FERRY',
+  },
+  {
+    value: 'ROUNDABOUT_CROSS',
+    display: 'ROUNDABOUT CROSS',
+  },
+  {
+    value: 'ROUNDABOUT_RIGHT',
+    display: 'ROUNDABOUT RIGHT',
+  },
+  {
+    value: 'ROUNDABOUT_LEFT',
+    display: 'ROUNDABOUT LEFT',
+  },
+  {
+    value: 'ROUNDABOUT_BACK',
+    display: 'ROUNDABOUT BACK',
+  },
+  {
+    value: 'TRY_MAKE_UTURN',
+    display: 'TRY MAKE_UTURN',
+  },
+  {
+    value: 'FOLLOW',
+    display: 'FOLLOW',
+  },
+  {
+    value: 'SWITCH_PARALLEL_ROAD',
+    display: 'SWITCH ROAD',
+  },
+  {
+    value: 'SWITCH_MAIN_ROAD',
+    display: 'SWITCH ROAD',
+  },
+  {
+    value: 'ENTRANCE_RAMP',
+    display: 'ENTRANCE RAMP',
+  },
+  {
+    value: 'WAYPOINT_LEFT',
+    display: 'WAYPOINT LEFT',
+  },
+  {
+    value: 'WAYPOINT_RIGHT',
+    display: 'WAYPOINT RIGHT',
+  },
+  {
+    value: 'WAYPOINT_REACHED',
+    display: 'WAYPOINT REACHED',
+  },
+];
