44dd8e36ab5bee4b36bed7a60a5cf6cb2590edff
Akhil <akhilnareshkumar@gmail.com>
2019-10-31 21:48:40 +0530

    Code cleanup/refactoring.

--- src/services/Navigation.js
+++ src/services/Navigation.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {Maneuvers} from '../constants/Maneuvers';
 
 const key = 'QxKUhPa8OHWrKshETgGXGsjEzPZOTiGE';
 
@@ -25,10 +26,47 @@ export const calculateRoute = async (from, to) => {
   }
 };
 
-export const createInstructionObj = (instruction, distance) => {
+export const createMockNavigationData = () => {
+  const maneuvers = Maneuvers;
+  const messages = [
+    'Leave from Ramganesh Gadkari Road',
+    'Turn right onto Kasaba Peth in 90 mts',
+    'Turn left onto Chhatrapati Shivaji Maharaj Road/NH4',
+  ];
+  const distances = [
+    '20 m',
+    '30 m',
+    '40 m',
+    '50 m',
+    '60 m',
+    '70 m',
+    '80 m',
+    '90 m',
+    '1.2 km',
+    '1.5 km',
+    '1.4 km',
+    '2.0 km',
+  ];
+  const angles = [45, 90, -45, -90];
+  const getRandom = array => {
+    const idx = Math.floor(Math.random() * (array.length - 1));
+    return array[idx];
+  };
+
+  const message = {
+    maneuver: getRandom(maneuvers).value,
+    display: getRandom(maneuvers).display,
+    message: getRandom(messages),
+    distance: getRandom(distances),
+    turnAngle: getRandom(angles),
+  };
+  return message;
+};
+
+export const createNavigationData = (instruction, distance) => {
   const messageObj = {
     maneuver: instruction.maneuver,
-    display: instruction.maneuver,
+    display: Maneuvers.find(m => m.value === instruction.maneuver).display,
     message: instruction.message,
     distance: distance + ' m',
     turnAngle: instruction.turnAngleInDecimalDegrees,
