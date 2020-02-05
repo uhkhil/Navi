4299f29fe926c9856b135a6ad1281a376857404f
Akhil Kumar <akhilnareshkumar@gmail.com>
2020-02-05 22:30:37 +0530

    Remove Setup screen for now.

--- src/screens/Landing/Landing.js
+++ src/screens/Landing/Landing.js
@@ -9,11 +9,8 @@ export class Landing extends React.Component {
 
   checkNextStep = async () => {
     const onboarding = await AsyncStorage.getItem('onboarding');
-    const setup = await AsyncStorage.getItem('setup');
     if (onboarding !== 'done') {
       this.props.navigation.navigate('Onboarding');
-    } else if (setup !== 'done') {
-      this.props.navigation.navigate('SetupInitial');
     } else {
       this.props.navigation.navigate('Main');
     }
