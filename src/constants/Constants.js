6db4aad557d4947938ebbe005dd1184946899060
Akhil Kumar <akhilnareshkumar@gmail.com>
2021-02-17 08:04:56 +0530

    Add new Search APIs
    - Integrated Google place/autocomplete API for faster and better search
    - Integrated place/details

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -11,6 +11,10 @@ export const Constants = {
     CALCULATE_ROUTE: '/routing/1/calculateRoute/',
   },
   API_KEY: '64TILwSA0P0DyYtwQd6BvOa8BsvBzSP4',
+  GOOGLE_AUTOCOMPLETE_API:
+    'https://maps.googleapis.com/maps/api/place/autocomplete/json',
+  GOOGLE_PLACES_API: 'https://maps.googleapis.com/maps/api/place/details/json',
+  GOOGLE_API_KEY: 'AIzaSyAkhaWEW-IdzpT8Xr89RUE8Flgq9LCiRvc',
   // TODO: Shift above things to the server
   BASE_URL: 'https://navicast-server.herokuapp.com',
   ENDPOINTS: {
